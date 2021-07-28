07fb63a0f20250afe6a164cd8e1311b4ec8afc78
xesam <xesamguo@gmail.com>
2021-07-29 00:00:11 +0800

    0.0.3

--- libs/Component.js
+++ libs/Component.js
@@ -6,10 +6,7 @@ function create(extra, factory = Component) {
     return function (options = {}) {
         options.externalClasses = defaultArray(extra.externalClasses).concat(defaultArray(options.externalClasses));
         options.behaviors = defaultArray(extra.behaviors).concat(defaultArray(options.behaviors));
-        options.options = {
-            ...extra.options,
-            ...options.options
-        }
+        options.options = Object.assign({}, extra.options || {}, options.options || {});
         factory(options);
     }
 }
